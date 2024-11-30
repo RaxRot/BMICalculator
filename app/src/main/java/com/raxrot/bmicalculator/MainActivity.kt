422252f4293679ef8d844cc64b6c209de9fbe034
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-11-30 13:31:26 +0000

    Edit TextInput

--- app/src/main/java/com/raxrot/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/raxrot/bmicalculator/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -20,6 +21,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.raxrot.bmicalculator.ui.theme.BMICalculatorTheme
@@ -40,8 +42,46 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BmiCalculatorPage() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
 
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "Bmi Calculator") }
+                )
+            }
+        ) {
+            var weight = remember { mutableStateOf("") }
+
+           Column(modifier = Modifier.padding(it)) {
+                EditNumberTextField(value =weight.value
+                    , label = "Weight (in kg)", keyBoardOptions =KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number
+                    ) ,
+                    onValueChanged ={weight.value=it} )
+
+           }
+        }
+    }
 }
+
+@Composable
+fun EditNumberTextField(value:String,label:String,keyBoardOptions:KeyboardOptions,
+                        onValueChanged:(String)->Unit) {
+
+    TextField(value = value,
+        onValueChange ={onValueChanged},
+        keyboardOptions = keyBoardOptions,
+        label = { Text(text = label)},
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp) )
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
